6c1153a28fa8c414829c12e8ae35ae7a6d1ead2e
harishbalan2101 <hb3398@srmist.edu.in>
2026-03-01 20:02:34 +0530

    sixth usecase

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,49 @@
-public class OOPSBannerApp{
-    public static void main(String[] args){
-        System.out.println("OOPS");
-    } 
+public class OOPSBannerApp {
+
+    public static String[] getOpattern() {
+        return new String[] {
+                "   ***   ",
+                " **   ** ",
+                "**     **",
+                "**     **",
+                "**     **",
+                " **   ** ",
+                "   ***   "
+        };
+    }
+
+    public static String[] getSpattern() {
+        return new String[] {
+                "   ***   ",
+                "**    ** ",
+                "**       ",
+                "  *****  ",
+                "      ** ",
+                "**    ** ",
+                "   ***   "
+        };
+    }
+
+    public static String[] getPpattern() {
+        return new String[] {
+                "********  ",
+                "**     ** ",
+                "**     ** ",
+                "********  ",
+                "**        ",
+                "**        ",
+                "**        ",
+        };
+    }
+
+    public static void main(String[] args) {
+        String[] oPattern = getOpattern();
+        String[] sPattern = getSpattern();
+        String[] pPattern = getPpattern();
+
+        for (int i=0;i< oPattern.length;i++){
+            System.out.println(oPattern[i] + " "+ oPattern[i] + " " + pPattern[i] + " " + sPattern[i]);
+        }
+
+    }
 }
